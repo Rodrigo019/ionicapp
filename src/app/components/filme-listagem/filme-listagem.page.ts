26a3d1fea644994feead9dafe3c93a786773ebda
Rodrigo Otávio Belo <rodrigootavio019@gmail.com>
2021-02-13 22:17:32 -0300

    Listagem de filmes lado a lado

--- src/app/components/filme-listagem/filme-listagem.page.ts
+++ src/app/components/filme-listagem/filme-listagem.page.ts
@@ -12,32 +12,53 @@ export class FilmeListagemPage implements OnInit {
   @Input() NomeDoFilme: string = null;  
 
   private FilmesMemoria: Filme[] = [];
-  private FilmesCarregados: Filme[] = [];
-  private Filmes: Filme[] = [];  
+  private Filmes: Filme[] = []; 
+  private MatrizDeFilmes: any[] = []; 
   private Index: number = 1;
 
   constructor(
     protected filmeService: FilmeService
   )
-  { 
-     this.filmeService.BuscarFilmesPopulares(true, this.Index).subscribe(
-       retorno => {
-        if (retorno)
-        {
-          retorno.results.forEach((x,i) => 
-          {
-            x.poster_path = `https://www.themoviedb.org/t/p/original${x.poster_path}`;
-            this.Filmes.push(x);
-          });
-          this.Index++;
-        }
-     },
-       erro => {
-         console.log(erro);
-     });
+  {      
   }
 
   ngOnInit() {
+
+    this.filmeService.BuscarFilmesPopulares(true, this.Index).subscribe(
+      retorno => {
+       if (retorno)
+       {
+        //  retorno.results.forEach((x,i) => 
+        //  {
+        //    x.poster_path = `https://www.themoviedb.org/t/p/original${x.poster_path}`;
+        //    this.Filmes.push(x);
+        //  });
+        //  this.Index++;
+
+        let cont = 0;
+        let filmesLinha: Filme[] = [];
+        
+        retorno.results.forEach(x => {
+          x.poster_path = `https://www.themoviedb.org/t/p/original${x.poster_path}`;
+          if (cont < 3)
+          {            
+            filmesLinha.push(x);
+            cont++;
+          }
+          else
+          {
+            this.MatrizDeFilmes.push(filmesLinha);
+            filmesLinha = [];
+            cont = 0;
+          }
+        });
+        this.Index++;
+       }
+    },
+     erro => {
+       console.log(erro);
+    });
+
     this.carregarFilmesMemoria();
   }
 
@@ -46,12 +67,33 @@ export class FilmeListagemPage implements OnInit {
     setTimeout(() => {
      this.filmeService.BuscarFilmesPopulares(true, this.Index).subscribe(
          retorno => {
-           retorno.results.forEach(x => 
-             {
-               x.poster_path = `https://www.themoviedb.org/t/p/original${x.poster_path}`;
-               this.Filmes.push(x);
-             });
-           this.Index++;
+           
+          let cont = 0;
+          let filmesLinha: Filme[] = [];
+
+          retorno.results.forEach(x => {
+            x.poster_path = `https://www.themoviedb.org/t/p/original${x.poster_path}`;
+            if (cont < 3)
+            {            
+              filmesLinha.push(x);
+              cont++;
+            }
+            else
+            {
+              this.MatrizDeFilmes.push(filmesLinha);
+              filmesLinha = [];
+              cont = 0;
+            }
+
+          });
+          this.Index++;
+          
+          //  retorno.results.forEach(x => 
+          //    {
+          //      x.poster_path = `https://www.themoviedb.org/t/p/original${x.poster_path}`;
+          //      this.Filmes.push(x);
+          //    });
+          //  this.Index++;
            $event.target.complete();
        }, 
          erro => {
@@ -75,16 +117,31 @@ export class FilmeListagemPage implements OnInit {
         erro => {
           console.log(erro);
       });
+      
     }
   }
 
   filtrar($event) {
+    this.MatrizDeFilmes = [];
     if ($event.length > 0)
     {
-      console.log($event);
-      this.FilmesCarregados = this.Filmes;
-      this.Filmes = $event;
-      this.Filmes.concat($event)
+      let cont = 0;
+      let filmesLinha: Filme[] = [];
+      $event.forEach(x => {
+        if (cont < 3)
+        {            
+          filmesLinha.push(x);
+          cont++;
+        }
+        else
+        {
+          this.MatrizDeFilmes.push(filmesLinha);
+          filmesLinha = [];
+          cont = 0;
+        }
+      });
+      //this.Filmes = $event;
+      //this.Filmes.concat($event)
     }
   }
 }
